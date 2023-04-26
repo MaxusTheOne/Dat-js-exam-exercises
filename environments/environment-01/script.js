312c941723888591deb5ea40f658c6fd5ffd6cb1
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-04-26 11:34:42 +0200

    ex 1

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,40 @@
 "use strict";
 
+window.addEventListener("load", initApp);
+
+async function initApp() {
+    const users = await getUsers();
+    console.log(users);
+    const admins = getAdmins(users);
+    showUsers(admins);
+}
+// del 1
+async function getUsers() {
+    const response = await fetch("users.json");
+    const data = await response.json();
+    return data;
+}
+
+// del 2
+function showUsers(users) {
+    for (const user of users) {
+        const html = /*html*/ `
+            <li>${user.name} - active: ${user.active}</li>
+        `;
+
+        document
+            .querySelector("#userlist")
+            .insertAdjacentHTML("beforeend", html);
+    }
+}
+
+// del 3
+function getAdmins(users) {
+    const results = users.filter(checkRole);
+
+    function checkRole(user) {
+        return user.role === "admin";
+    }
+
+    return results;
+}
